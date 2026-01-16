5de2520c0c31f107a62576a69b3cf653f133c02a
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-01-16 09:25:13 -0500

    Changed logging configuration
    Now I am using logging.config.dictConfig in order to set the
    configuration (formatters and handlers) for the root logger.

--- be2/src/app/config.py
+++ be2/src/app/config.py
@@ -1,4 +1,6 @@
 import logging
+import logging.config
+import logging.handlers
 
 from pydantic import Field, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -16,9 +18,47 @@ ENV = "dev"
 USERNAME = "appuser"
 LOG_PATH = f"/home/{USERNAME}/hello.log"
 
-log_level: int = logging.INFO
+LOG_LEVEL: int = logging.INFO
 
+# NOTE: Consider not making LOG_LEVEL dependend on environment variable
 if ENV == "dev":
-    log_level = logging.DEBUG
+    LOG_LEVEL = logging.DEBUG
 
-logging.basicConfig(filename=LOG_PATH, level=log_level)
+LOG_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {"format": "%(asctime)s::%(levelname)s::%(name)s::%(message)s"}
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+            "level": LOG_LEVEL,
+        }
+    },
+    "root": {"handlers": ["console"], "level": LOG_LEVEL},
+}
+
+
+def configure_logging():
+    logging.config.dictConfig(LOG_CONFIG)
+
+
+def get_custom_logger(logger_name: str):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(LOG_LEVEL)
+
+    stdout_handler = logging.StreamHandler()
+    file_handler = logging.handlers.RotatingFileHandler(
+        filename=LOG_PATH, maxBytes=1048576, backupCount=2
+    )
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
+
+    formatter = logging.Formatter(
+        ("%(asctime)s - %(levelname)s - %(name)s - " "%(message)s")
+    )
+    stdout_handler.setFormatter(formatter)
+
+    return logger
