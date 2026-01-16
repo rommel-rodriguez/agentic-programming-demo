dd1c8040c78f4341494ffdcdea259b06c3df9e54
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-01-16 09:26:55 -0500

    Configured logging config to be enforced at app start up
    Also, renamed the FastAPI object from app to fapi_app in order not
    confuse it with the name of the general module/project.

--- be2/src/app/main.py
+++ be2/src/app/main.py
@@ -4,49 +4,53 @@ from pathlib import Path
 import anyio
 from fastapi import FastAPI, File, HTTPException, UploadFile, status
 
-from app import bootstrap
+from app import bootstrap, config
 
-app = FastAPI()
+config.configure_logging()
+
+fapi_app = FastAPI()
 
 UPLOAD_ROOT = Path("uploads")
 CHUNK_SIZE = 1 * 1024 * 1024  # 1 MiB
 
 d1 = {"key1": 12, "key2": "Aufwiedersehen"}
 
+logger = logging.getLogger(__name__)
 
-@app.get("/test-string")
+
+@fapi_app.get("/test-string")
 def test_endpoint():
-    logging.info("test-string endpoint triggered")
+    logger.info("test-string endpoint aufwiedersehen")
     return "Hallo FastAPI world!"
 
 
-@app.get("/test-dict")
+@fapi_app.get("/test-dict")
 def test_dictionary():
-    logging.error("test-dict endpoint triggered")
+    logger.error("test-dict endpoint triggered")
     return d1
 
 
-@app.get("/test-debug")
+@fapi_app.get("/test-debug")
 def test_debug():
-    logging.debug("Is this printed while in INFO logging level?")
+    logger.debug("Is this printed while in INFO logger level?")
     return d1
 
 
-@app.get("/test-warning")
+@fapi_app.get("/test-warning")
 def test_warning():
-    logging.warning("Is this Warning logged at INFO level logging?")
+    logger.warning("Is this Warning logged at INFO level logger?")
     return d1
 
 
-@app.get("/test-exception")
+@fapi_app.get("/test-exception")
 def test_exception():
     def divide(a, b):
         try:
             result = a / b
         except ZeroDivisionError:
-            logging.exception("Hey Ho!")
+            logger.exception("Hey Ho!")
         except TypeError:
-            logging.exception("hey TE!")
+            logger.exception("hey TE!")
         else:
             return result
 
