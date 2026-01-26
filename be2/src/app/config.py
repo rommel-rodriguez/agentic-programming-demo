7a5ec7c206c7491d0846338bf9a79b019a830751
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-01-26 17:54:32 -0500

    Removed unused environment variables from Settings
    Also removed the environment-file based configuration in the
    application. I now using compose (or K8s in the future) to set the
    environment variables.

--- be2/src/app/config.py
+++ be2/src/app/config.py
@@ -9,12 +9,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=Path(__file__).resolve().parent / ".env", env_file_encoding="utf-8"
-    )
+    # model_config = SettingsConfigDict(
+    #     env_file=Path(__file__).resolve().parent / ".env", env_file_encoding="utf-8"
+    # )
 
-    debug: bool = False
-    database_url: Union[PostgresDsn, None] = None
+    # debug: bool = False
+    # database_url: Union[PostgresDsn, None] = None
     tavily_api_key: str = ""
     gemini_api_key: SecretStr
 
