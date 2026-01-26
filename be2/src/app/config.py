0e7d9d15bc2aae969337abcceeb7ce01307e9fc3
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-01-26 17:48:24 -0500

    Agent workflow working as expected
    The environment-variable parsing is failing in the be2 app workflow tho.

--- be2/src/app/config.py
+++ be2/src/app/config.py
@@ -1,16 +1,25 @@
 import logging
 import logging.config
 import logging.handlers
+from pathlib import Path
+from typing import Union
 
-from pydantic import Field, PostgresDsn
+from pydantic import Field, PostgresDsn, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict()
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).resolve().parent / ".env", env_file_encoding="utf-8"
+    )
 
     debug: bool = False
-    database_url: PostgresDsn
+    database_url: Union[PostgresDsn, None] = None
+    tavily_api_key: str = ""
+    gemini_api_key: SecretStr
+
+
+settings = Settings()
 
 
 # NOTE: Replace most, if not all, with environment variables
--- be2/src/app/main.py
+++ be2/src/app/main.py
@@ -1,8 +1,38 @@
 import logging
 from pathlib import Path
 
+from langchain_core.messages import (
+    AIMessage,
+    AnyMessage,
+    HumanMessage,
+    SystemMessage,
+    ToolMessage,
+)
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+from app.adapters.langgraph_agent import LangGraphAgent
+from app.config import settings
+
 # from app import bootstrap, config
 from app.entrypoints import webapp
+
+tool = TavilySearch(max_results=2, tavily_api_key=settings.tavily_api_key)
+# memory = SqliteSaver.from_conn_string(":memory:")
+prompt = """You are a smart research assistant. Use the search engine to look up information. \
+You are allowed to make multiple calls (either together or in sequence). \
+Only look up information when you are sure of what you want. \
+ATTENTION: If you need to look up some information before asking a follow up question, you are allowed to do that! \
+you have access to tools the tavily search tool amongs them.
+IMPORTANT: Make sure to review the tools available to you before making a judgement. \
+Also, plan the order of tool calls and reasoning before all else.
+"""
+MODEL = "gemini-2.5-flash"
+model = ChatGoogleGenerativeAI(model=MODEL)
+model.google_api_key = settings.gemini_api_key
+messages = [HumanMessage(content="What is the weather in San Francisco?")]
+
 
 # import anyio
 
@@ -58,3 +88,18 @@ def test_exception():
 
     result = divide(1, 0)
     return result
+
+
+@app.get("/query-lgmodel")
+async def query_lgmodel(query: str):
+    agent_state = None
+    with SqliteSaver.from_conn_string(":memory:") as memory:
+        abot = LangGraphAgent(
+            model, [tool], thread_id="3", system=prompt, checkpointer=memory
+        )
+        agent_state = abot.query_stream(query)
+
+    result = agent_state.values["messages"][-1].text
+    if not result:
+        pass  # NOTE: Raise/return some error here.
+    return {"result": result}
