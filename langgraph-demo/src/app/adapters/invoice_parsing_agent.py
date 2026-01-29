d6f355d5237055fbde907cfc10b9f4f3c62608dd
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-01-29 15:59:42 -0500

    Add: stub of agent
    The agent stub is meant to be an invoice-parsing agent.

---
+++ langgraph-demo/src/app/adapters/invoice_parsing_agent.py
@@ -0,0 +1,81 @@
+import logging
+import operator
+from typing import Annotated, TypedDict
+
+from langchain_core.messages import (
+    AIMessage,
+    AnyMessage,
+    HumanMessage,
+    SystemMessage,
+    ToolMessage,
+)
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph import END, StateGraph
+
+logger = logging.getLogger(__name__)
+
+MODEL = "gemini-2.5-flash"
+
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+
+
+class InvoiceAgentState(TypedDict):
+    task: str
+    plan: str
+    draft: str
+    critique: str
+    content: list[str]
+    revision_number: int
+    max_revisions: int
+
+
+class InvoiceParsingAgent:
+    def __init__(self, model, tools, checkpointer, thread_id: str, system: str = ""):
+        self.system = system
+        self.model = model.bind_tools(tools)
+        self.thread_id = thread_id
+        graph_builder = StateGraph(AgentState)
+        graph_builder.add_node("llm", self.call_model)
+        graph_builder.add_conditional_edges(
+            "llm", self.exists_action, {True: "action", False: END}
+        )
+        graph_builder.add_node("action", self.execute_action)
+        graph_builder.add_edge("action", "llm")
+        graph_builder.set_entry_point("llm")
+        self.graph = graph_builder.compile(checkpointer=checkpointer)
+        self.tools = {t.name: t for t in tools}
+
+    def call_model(self, state: AgentState):
+        messages = state["messages"]
+        if self.system:
+            messages = [SystemMessage(content=self.system)] + messages
+        message = self.model.invoke(messages)
+        return {"messages": [message]}
+
+    def execute_action(self, state: AgentState):
+        tool_calls = state["messages"][-1].tool_calls
+        results = []
+        for t in tool_calls:
+            logger.info(f"Calling {t}")
+            result = self.tools[t["name"]].invoke(t["args"])
+            results.append(
+                ToolMessage(tool_call_id=t["id"], name=t["name"], content=str(result))
+            )
+        logger.info(f"Finished executing action/s. Going back to the model")
+        return {"messages": results}
+
+    def exists_action(self, state: AgentState):
+        result = state["messages"][-1]
+        return len(result.tool_calls) > 0
+
+    def query_stream(self, input_query: str | None):
+        messages = [HumanMessage(content=input_query)]
+        thread = {"configurable": {"thread_id": self.thread_id}}
+
+        for event in self.graph.stream({"messages": messages}, thread):
+            for v in event.values():
+                logger.info(v["messages"])
+
+        return self.graph.get_state(thread)
