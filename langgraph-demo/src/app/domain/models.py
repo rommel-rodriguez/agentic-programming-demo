ccc23d0cc90bed9617013485fe261a2dd13af2ca
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-02-10 12:39:03 -0500

    Modify: domain models, refactoring and adding
    Most models have been turned to dataclass models, and a new Invoice
    model has been add.

--- langgraph-demo/src/app/domain/models.py
+++ langgraph-demo/src/app/domain/models.py
@@ -1,21 +1,30 @@
+from datetime import date
 from typing import Optional
 
+from attr import dataclass
 
+
+@dataclass
 class User:
     id: int
     name: str
     email: str
 
-    def __init__(self, id: int, name: str, email: str):
-        self.id = id
-        self.name = name
-        self.email = email
 
-
+@dataclass
 class Message:
     content: str
 
 
+@dataclass
 class Thread:
     user_id: int
     messages: list[Message]
+
+
+@dataclass
+class Invoice:
+    amount: float
+    currency: str
+    emission_date: date
+    due_date: date
