dbdd0db7dc075aa57e3124cc98b6e92ae7a77f6d
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-02-14 08:38:58 -0500

    Add: Postres connection pool and pointer
    Adds Postresql connection pool configuration and object, creates a
    PostgresSaver checkpointer and attaches both pool and pointer to
    app.state. Which is a valid, if messy approach.
    
    TODO: Heavily refactor this, there are too many things being defined in
    app_factory.py and no flexibility being gained, either for development
    or for tests.

--- langgraph-demo/src/app/bootstrap/app_factory.py
+++ langgraph-demo/src/app/bootstrap/app_factory.py
@@ -4,18 +4,41 @@ from contextlib import asynccontextmanager
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler
+from langgraph.checkpoint.postgres import PostgresSaver
+from psycopg import Connection
+from psycopg.rows import DictRow, dict_row
+from psycopg_pool import ConnectionPool
 
 from app.bootstrap.logging import configure_logging
+from app.config import settings
 from app.entrypoints.webapp.routers.invoice import router as invoice_router
 from app.entrypoints.webapp.routers.workflows import router as wf_router
 
 logger = logging.getLogger(__name__)
 
 
+# NOTE: Consider moving this inside create_app, and use create_app as a closure
+# in order to configure the type of checkpointer that the application uses
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     configure_logging()
-    yield
+    db_url = str(settings.db_url)
+    logger.info(f"Starting db with db_url: {settings.db_url}")
+    pool = ConnectionPool(
+        db_url,
+        max_size=10,
+        connection_class=Connection[DictRow],  # Needed ore mypy complains
+        kwargs={"autocommit": True, "row_factory": dict_row},
+    )
+    checkpointer = PostgresSaver(pool)
+    checkpointer.setup()  # Creates required tables for checkpointing for the first time
+
+    app.state.checkpointer = checkpointer
+    app.state.pg_pool = pool
+    try:
+        yield
+    finally:
+        pool.close()
 
 
 async def http_exception_handle_logging(request, exc):
