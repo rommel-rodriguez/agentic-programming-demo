7904c0cf515787f92b7b1dc9aaa047f925828751
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-02-15 12:46:50 -0500

    Refactor: workflows endpoint
    It will now use FastAPI DI mechanism (Depends) to inject the correctly
    configured agent for the request context.

--- langgraph-demo/src/app/entrypoints/webapp/routers/workflows.py
+++ langgraph-demo/src/app/entrypoints/webapp/routers/workflows.py
@@ -1,48 +1,28 @@
 import logging
 
-from fastapi import APIRouter, HTTPException
-from langchain_core.messages import HumanMessage
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_tavily import TavilySearch
-from langgraph.checkpoint.sqlite import SqliteSaver
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.adapters.langgraph_agent import LangGraphAgent
-from app.config import settings  # type:ignore
+from app.entrypoints.webapp.dependencies import get_query_agent_with_search
 from app.entrypoints.webapp.models.workflows import LGQuery
 
 logger = logging.getLogger(__name__)
-tool = TavilySearch(max_results=2, tavily_api_key=settings.tavily_api_key)
-# memory = SqliteSaver.from_conn_string(":memory:")
-prompt = """You are a smart research assistant. Use the search engine to look up information. \
-You are allowed to make multiple calls (either together or in sequence). \
-Only look up information when you are sure of what you want. \
-ATTENTION: If you need to look up some information before asking a follow up question, you are allowed to do that! \
-you have access to tools the tavily search tool amongs them.
-IMPORTANT: Make sure to review the tools available to you before making a judgement. \
-Also, plan the order of tool calls and reasoning before all else.
-"""
-MODEL = "gemini-2.5-flash"
-model = ChatGoogleGenerativeAI(model=MODEL)
-model.google_api_key = settings.gemini_api_key
-messages = [HumanMessage(content="What is the weather in San Francisco?")]
 
 
 router = APIRouter(tags=["agent-workflows"])
 
 
 @router.get("/query-lgmodel", response_model=LGQuery)
-async def query_lgmodel(query: str):
-    logger.info("Langgraph test endpoint reached")
-    agent_state = None
-    result = dict()
-    with SqliteSaver.from_conn_string(":memory:") as memory:
-        abot = LangGraphAgent(
-            model, [tool], thread_id="3", system=prompt, checkpointer=memory
-        )
-        result = await abot(query)
+async def query_lgmodel(
+    query: str,
+    thread_id: str,
+    agent: LangGraphAgent = Depends(get_query_agent_with_search),
+):
+    logger.info("Langgraph endpoint reached")
+    result = await agent(query, thread_id)
 
     logger.debug(f"Query output: {result}")
-    if not result:
+    if not result.get("result"):
         raise HTTPException(
             status_code=404, detail="The agent did not return a valid response"
         )
