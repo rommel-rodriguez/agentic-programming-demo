f07f5d6f88bfd05a1125d2f916cdb1b4aa58237e
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-02-16 10:26:21 -0500

    Add: fixtures to conftest
    This fixture are app-configuration and database related, two build the
    different version of the app, each using a different checkpointer
    (in-memory vs Postgres backed). Then we likewise build two clients (sync
    and async) for each of type of app configured.

--- langgraph-demo/tests/conftest.py
+++ langgraph-demo/tests/conftest.py
@@ -7,25 +7,21 @@ from httpx import ASGITransport
 
 from app.bootstrap.app_factory import create_app
 
-TEST_BASE_URL = 'http://testserver' 
+TEST_BASE_URL = "http://testserver"
 
 # NOTE: Or should I import this from entrypoints.webapp.asgi?
 # Or the current approach is better, as I can build the app differently
 # for tests?
-app = create_app()
 
-@pytest.fixture(scope="session")
-def anyio_backend():
-    return "asyncio"
 
 @pytest.fixture()
-def base_url()
-    return TEST_BASE_URL
+def app_memory():
+    return create_app(checkpointer_backend="memory")
+
 
 @pytest.fixture()
-def client(base_url):
-    with TestClient(app, base_url=base_url) as c:
-        yield c
+def app_postgres():
+    return create_app(checkpointer_backend="memory")
 
 
 # @pytest.fixture()
@@ -34,8 +30,38 @@ def client(base_url):
 #     yield
 #     await database.disconnect()
 
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
 @pytest.fixture()
-async def async_client(base_url):
-    transport = ASGITransport(app=app)
+def base_url():
+    return TEST_BASE_URL
+
+
+@pytest.fixture()
+def client_memory(app_memory, base_url):
+    with TestClient(app_memory, base_url=base_url) as c:
+        yield c
+
+
+@pytest.fixture()
+def client_postgres(app_postgres, base_url):
+    with TestClient(app_memory, base_url=base_url) as c:
+        yield c
+
+
+@pytest.fixture()
+async def async_client_memory(app_memory, base_url):
+    transport = ASGITransport(app=app_memory)
+    async with httpx.AsyncClient(transport=transport, base_url=base_url) as ac:
+        yield ac
+
+
+@pytest.fixture()
+async def async_client_postgres(app_postgres, base_url):
+    transport = ASGITransport(app=app_memory)
     async with httpx.AsyncClient(transport=transport, base_url=base_url) as ac:
         yield ac
