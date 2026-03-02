d9edd0b659722d4bc8e69050d68b230dce67aff0
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-03-02 11:32:59 -0500

    Add: command UploadAttachmentContentCommand
    This command will be used by a new service.

--- langgraph-demo/src/app/services/commands.py
+++ langgraph-demo/src/app/services/commands.py
@@ -8,3 +8,11 @@ class Command:
 @dataclass(frozen=True)
 class RunLGWorkflowCommand(Command):
     query: str
+
+
+@dataclass(frozen=True, slots=True)
+class UploadAttachmentContentCommand(Command):
+    attachment_id: str
+    content_type: str
+    content: bytes
+    original_filename: str | None = None
