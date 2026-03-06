ae00f4f946b01d863a677857e34f6e2527512764
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-03-05 21:32:49 -0500

    Add: application level errors

---
+++ langgraph-demo/src/app/services/errors.py
@@ -0,0 +1,18 @@
+class ApplicationError(Exception):
+    code = "application_error"
+
+
+class UnsupportedMimeTypeError(ApplicationError):
+    code = "unsupported_mime_type"
+
+
+class AttachmentNotPendingError(ApplicationError):
+    code = "attachment_not_pending"
+
+
+class StorageUnavailableError(ApplicationError):
+    code = "storage_unavailable"
+
+
+class AttachmentMetadataUpdateError(ApplicationError):
+    code = "attachment_metadata_update_error"
