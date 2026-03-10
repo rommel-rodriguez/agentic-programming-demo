447a071a0e9ca827cf9432fbc0ca5e1e60fa3ec6
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-03-09 20:18:00 -0500

    Add: function to build a session factory

--- langgraph-demo/src/app/bootstrap/persistence.py
+++ langgraph-demo/src/app/bootstrap/persistence.py
@@ -1,4 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from app import config
 from app.adapters.orm import start_mappers
+
+
+def build_session_factory():
+    db_url = str(config.get_settings().db_url_sqlalchemy)
+    return async_sessionmaker(
+        bind=create_async_engine(
+            url=db_url, isolation_level="REPEATABLE READ", pool_pre_ping=True
+        )
+    )
 
 
 async def configure_persistence(start_orm: bool = True):
