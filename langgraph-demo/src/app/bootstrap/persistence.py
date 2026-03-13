786f86b39706ef8f7f7d07ff9364bf00aa3b2428
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-03-13 10:30:09 -0500

    Refactor: improves SQLAlchemy engine setup
    This update makes the setup of the engine richer and more specific, for
    this purpose it introduces a helper function exclusively to build the
    engine.

--- langgraph-demo/src/app/bootstrap/persistence.py
+++ langgraph-demo/src/app/bootstrap/persistence.py
@@ -1,17 +1,38 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 from app import config
 from app.adapters.orm import start_mappers
 
 
+def build_engine() -> AsyncEngine:
+    db_url = str(config.get_settings().db_url_sqlalchemy)
+    return create_async_engine(
+        db_url,
+        isolation_level="REPEATABLE READ",
+        pool_pre_ping=True,
+        pool_size=20,
+        max_overflow=0,
+        pool_timeout=5,
+        pool_recycle=3600,
+        pool_use_lifo=True,
+    )
+
+
 def build_session_factory():
     db_url = str(config.get_settings().db_url_sqlalchemy)
+    engine = build_engine()
     return async_sessionmaker(
-        bind=create_async_engine(
-            url=db_url, isolation_level="REPEATABLE READ", pool_pre_ping=True
-        )
+        bind=engine,
+        autoflush=False,
+        expire_on_commit=False,
     )
 
 
 async def configure_persistence(start_orm: bool = True):
-    start_mappers()
+    if start_orm:
+        start_mappers()
