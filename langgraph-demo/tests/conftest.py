02582dc4afd313871ed76acefd1277135a586811
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-03-14 19:27:37 -0500

    Add: fixtures for integration testing
    Includes fixtures for creation of in-memory and Postgres engines and
    session factories.

--- langgraph-demo/tests/conftest.py
+++ langgraph-demo/tests/conftest.py
@@ -1,8 +1,13 @@
 import httpx
 import pytest
+import pytest_asyncio
 from fastapi.testclient import TestClient
 from httpx import ASGITransport
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import clear_mappers
 
+from app import config
+from app.adapters.orm import mapper_registry, start_mappers
 from app.bootstrap.app_factory import create_app
 
 TEST_BASE_URL = "http://testserver"
@@ -12,17 +17,47 @@ TEST_BASE_URL = "http://testserver"
 # for tests?
 
 
-@pytest.fixture()
-def in_memory_db():
-    return create_app(checkpointer_backend="memory")
+@pytest.fixture(scope="session")
+def mappers():
+    start_mappers()
+    yield
+    clear_mappers()
 
 
-@pytest.fixture()
+@pytest_asyncio.fixture()
+async def in_memory_db():
+    engine = create_async_engine("sqlite+aiosqlite:///:memory:")
+    # mapper_registry.metadata.create_all(engine)
+    async with engine.begin() as conn:
+        await conn.run_sync(mapper_registry.metadata.create_all)
+    return engine
+
+
+@pytest.fixture
+def in_memory_session_factory(in_memory_db):
+    return async_sessionmaker(in_memory_db)
+
+
+@pytest.fixture
+def postgres_db():
+    db_url_sqlalchemy: str = str(config.get_settings().db_url_sqlalchemy)
+    engine = create_async_engine(db_url_sqlalchemy, isolation_level="SERIALIZABLE")
+    # NOTE: If we add code to wait for postgres here, this fixture would need to become
+    # an async fixture.
+    return engine
+
+
+@pytest.fixture
+def postgres_session_factory(postgres_db):
+    return async_sessionmaker(bind=postgres_db)
+
+
+@pytest.fixture
 def app_memory():
     return create_app(checkpointer_backend="memory")
 
 
-@pytest.fixture()
+@pytest.fixture
 def app_postgres():
     return create_app(checkpointer_backend="postgres")
 
@@ -32,32 +67,32 @@ def anyio_backend():
     return "asyncio"
 
 
-@pytest.fixture()
+@pytest.fixture
 def base_url():
     return TEST_BASE_URL
 
 
-@pytest.fixture()
+@pytest.fixture
 def client_memory(app_memory, base_url):
     with TestClient(app_memory, base_url=base_url) as c:
         yield c
 
 
-@pytest.fixture()
+@pytest.fixture
 def client_postgres(app_postgres, base_url):
-    with TestClient(app_memory, base_url=base_url) as c:
+    with TestClient(app_postgres, base_url=base_url) as c:
         yield c
 
 
-@pytest.fixture()
+@pytest_asyncio.fixture
 async def async_client_memory(app_memory, base_url):
     transport = ASGITransport(app=app_memory)
     async with httpx.AsyncClient(transport=transport, base_url=base_url) as ac:
         yield ac
 
 
-@pytest.fixture()
+@pytest_asyncio.fixture
 async def async_client_postgres(app_postgres, base_url):
-    transport = ASGITransport(app=app_memory)
+    transport = ASGITransport(app=app_postgres)
     async with httpx.AsyncClient(transport=transport, base_url=base_url) as ac:
         yield ac
